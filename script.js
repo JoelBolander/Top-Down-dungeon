1449d7e56f8bc11b1acfee8f653e25960726cfcc
hampusrydh <hampus.rydh@skola.taby.se>
2023-04-01 23:49:06 +0200

    smooth movement function + general improvments

--- script.js
+++ script.js
@@ -1,40 +1,123 @@
 window.focus;
-let canvas = document.getElementById("myCanvas");
-let c = canvas.getContext("2d");
-canvas.height = innerHeight * 0.8
-canvas.width = canvas.height * 1.5
+const CANVAS = document.getElementById("myCanvas");
+const CTX = CANVAS.getContext("2d");
+CANVAS.height = innerHeight * 0.8;
+CANVAS.width = CANVAS.height * 1.5;
 
-let tilesize = canvas.width * 0.01
+const TILESIZE = CANVAS.width * 0.065;
+let acceleration = 0.4;
 
-let blob = {x: 5*tilesize,
-    y: 5*tilesize,
-    size: tilesize,
-    movingUp: false,
-    movingDown: false,
-    movingLeft: false,
-    movingRight: false,
+let player = {
+  pos: [2 * TILESIZE, 2 * TILESIZE],
+  size: TILESIZE,
+  vel: [0, 0], // velocity unit - millitiles per frame
+  maxVel: 9,
+  acc: [0, 0],
+};
+
+function move(obj) {
+  let targetVelX;
+  let targetVelY;
+
+  // find desired velocity (the velocity the player will reach with the current keys pressed)
+  // 1. if no direction pressed
+  if (!(obj.up ^ obj.down || obj.left ^ obj.right)) {
+    targetVelX = targetVelY = 0;
+  }
+  // 2. if one direction pressed
+  else if (obj.up ^ obj.down ^ (obj.left ^ obj.right)) {
+    if (obj.up && !obj.down) {
+      targetVelX = 0;
+      targetVelY = -obj.maxVel;
+    }
+    if (obj.left && !obj.right) {
+      targetVelX = -obj.maxVel;
+      targetVelY = 0;
+    }
+    if (obj.down && !obj.up) {
+      targetVelX = 0;
+      targetVelY = obj.maxVel;
+    }
+    if (obj.right && !obj.left) {
+      targetVelX = obj.maxVel;
+      targetVelY = 0;
+    }
+  }
+  // 3. if two directions pressed (diagonal)
+  else {
+    if (obj.up && !obj.down) {
+      targetVelY = -obj.maxVel / Math.sqrt(2);
+    }
+    if (obj.left && !obj.right) {
+      targetVelX = -obj.maxVel / Math.sqrt(2);
+    }
+    if (obj.down && !obj.up) {
+      targetVelY = obj.maxVel / Math.sqrt(2);
+    }
+    if (obj.right && !obj.left) {
+      targetVelX = obj.maxVel / Math.sqrt(2);
+    }
+  }
+
+  // calculate acceleration based on distance to desired velocity
+  obj.acc[0] = (targetVelX - obj.vel[0]) * acceleration;
+  obj.acc[1] = (targetVelY - obj.vel[1]) * acceleration;
+
+  // apply acceleration and velocity
+  obj.vel[0] += obj.acc[0];
+  obj.vel[1] += obj.acc[1];
+
+  obj.pos[0] += player.vel[0] * TILESIZE * 0.01;
+  obj.pos[1] += player.vel[1] * TILESIZE * 0.01;
 }
 
 document.addEventListener("keydown", (e) => {
-    if(e.key == "w"){blob.movingUp = true}
-    if(e.key == "a"){blob.movingLeft = true}
-    if(e.key == "s"){blob.movingDown = true}
-    if(e.key == "d"){blob.movingRight = true}
+  if (e.repeat) {
+    return;
+  }
+  if (e.key === "w") {
+    player.up = true;
+  }
+  if (e.key === "a") {
+    player.left = true;
+  }
+  if (e.key === "s") {
+    player.down = true;
+  }
+  if (e.key === "d") {
+    player.right = true;
+  }
 });
+
 document.addEventListener("keyup", (e) => {
-    if(e.key == "w"){blob.movingUp = false}
-    if(e.key == "a"){blob.movingLeft = false}
-    if(e.key == "s"){blob.movingDown = false}
-    if(e.key == "d"){blob.movingRight = false}
+  if (e.key === "w") {
+    player.up = false;
+  }
+  if (e.key === "a") {
+    player.left = false;
+  }
+  if (e.key === "s") {
+    player.down = false;
+  }
+  if (e.key === "d") {
+    player.right = false;
+  }
 });
 
 function animate() {
-    requestAnimationFrame(animate);
+  move(player);
 
-    c.clearRect(0,0, canvas.width, canvas.height)
-    c.fillRect(blob.x - blob.size/2,blob.y - blob.size/2,blob.x + blob.size/2,blob.y + blob.size/2)
+  CTX.clearRect(0, 0, CANVAS.width, CANVAS.height);
 
-    
+  // draw player
+  CTX.fillRect(
+    player.pos[0] - player.size / 2,
+    player.pos[1] - player.size / 2,
+    player.size,
+    player.size
+  );
+
+  requestAnimationFrame(animate);
 }
 
-animate()
+animate();
